d7d518066a5fae7a444a7f64b74327118d8cb29e
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-18 19:56:56 +0100

    Add task0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""The module contains pagination exercise"""
+
+
+def index_range(page, page_size):
+    """returns start and end index"""
+    start_index = page * page_size - 1
+    end_index = start_index + page_size
+    return (start_index, end_index)
