80a1568edda7994795031d166f0ce9177273b93f
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-19 12:04:51 +0100

    Add task

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,12 +5,6 @@ import math
 from typing import List
 
 
-def index_range(page, page_size):
-    """returns start and end index"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
