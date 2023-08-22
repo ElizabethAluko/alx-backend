64c9fc07a1118915db55e9169215311817ade3d5
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-22 21:37:25 +0100

    Add task1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Base Dictionary Module"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Basic dictionary Caching"""
+
+    def __init__(self):
+        """Initialize the class with the super class init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Set the nee value"""
+        if key != None and item != None:
+            self.cache_data[key] = item
+        if len(self.cache_data) > super().MAX_ITEMS:
+            key, item = next(iter(self.cache_data.items()))
+            del self.cache_data[key]
+            print('DISCARD: {}'.format(key))
+
+    def get(self, key):
+        """Retrieve the value of the given key"""
+        if key in self.cache_data:
+            return(self.cache_data[key])
+        else:
+            return None
