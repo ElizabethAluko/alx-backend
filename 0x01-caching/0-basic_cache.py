ec96f252d5aef7e2dbcf87322d5733778494c9a4
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-22 18:58:54 +0100

    Add task

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Base Dictionary Module"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic dictionary Caching"""
+
+    def __init__(self):
+        """Initialize the class with the super class init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Set the nee value"""
+        if key != None and item != None:
+            self.cache_data[key] = item
+    def get(self, key):
+        """Retrieve the value of the given key"""
+        if key in self.cache_data:
+            return(self.cache_data[key])
+        else:
+            return None
+
