846fcf57f98dfbc8745531cdf878e46d03a07077
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-01 22:18:05 +0100

    Add task

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Babel Setup"""
 
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -16,19 +16,3 @@ class Config:
 
 
 app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale():
-    """Get the local languages from sys or browser or client settings"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index():
-    """Renders index page to the browser"""
-    return render_template('1-index.html')
-
-
-if __name__ == '__main__':
-    1-app.run()
