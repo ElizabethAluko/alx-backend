a106ff16c0fe9fed4914983e5297873fd86fa697
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-01 21:30:47 +0100

    Add task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Renders the index page to the browser"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     0-app.run()
